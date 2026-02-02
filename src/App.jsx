47b804b13d7bba862fde1487a1a77b5921bf499c
KrishnaUmradiya <krishnaumradiya81@gmail.com>
2026-02-02 13:30:30 +0530

    [KU] [update success]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+const DefaultRoute = () => {
+  const authData = JSON.parse(localStorage.getItem("authData"));
+  if (authData) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Navigate to="/register" replace />;
+};
+
 function App() {
   const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <DefaultRoute />,
+    },
     {
       path: "/login",
       element: <Login />,
